d1ad47a04c991827129c1c43d47002c575b780ff
Arsalaann <arsalankhanxk@gmail.com>
2023-04-26 11:21:42 +0530

    Comparision Detailed

--- CompareLine.cs
+++ CompareLine.cs
@@ -4,9 +4,6 @@ namespace LineComparisionComputation{
         private static int Pow(int num){
             return num*num;
         }
-        private static bool CheckEquality(int L1,int L2){
-            return L1==L2;
-        }
         public static void Compare(){
             int x1,y1,x2,y2;
             int X1,Y1,X2,Y2;
@@ -23,10 +20,12 @@ namespace LineComparisionComputation{
             int Lth2=Convert.ToInt32(Math.Sqrt(Pow(Math.Abs(X2-X1))+Pow(Math.Abs(Y2-Y1))));
             Console.WriteLine("Line one Length : "+Lth1);
             Console.WriteLine("Line two Length : "+Lth2);
-            if(CheckEquality(Lth1, Lth2))
-                Console.WriteLine("Line are Equal");
+            if(Lth1==Lth2)
+                Console.WriteLine("Both Lines are Equal");
+            else if(Lth1>Lth2)
+                Console.WriteLine("Line 1 is greater than line 2");
             else
-                Console.WriteLine("Line are not Equal");
+                Console.WriteLine("Line 2 is greater than line 1");
         }
     }
 }
